7a1a53fc333826fe50484838692740969485d1af
Dan Stoner <danstoner@gmail.com>
2018-05-26 09:13:53 -0400

    builk add existing dynatable bits

---
+++ dynatable/running_entries.js
@@ -0,0 +1,32 @@
+  console.log("Hello World with no function!");
+
+  $(document).ready(function() {
+  // all custom jQuery will go here
+
+  console.log("IN document ready function");
+
+  $.getJSON("/static/danstoner_dailymile_running_entries_postETL_nokeys.json", function (data) {
+    console.log("got it");
+$('#entries-table').dynatable({
+    dataset: {
+	   totalRecordCount: true,
+	   perPageOptions: [25,100,500],
+           records: data
+
+        },
+    writers: {
+	'url': function(v) {
+	    return '<a href="' + v.url + '">' + v.url + '</a>';
+	    }
+	},
+    table: {
+	defaultColumnIdStyle: 'underscore'
+    }
+})
+
+  })
+  .fail(function () {
+      console.log('error reading or parsing JSON');
+});
+
+});
